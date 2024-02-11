e9dd088394606da6e506d1e5730fe0f2c90b1321
yassino <letzyassinehd@gmail.com>
2024-02-11 13:52:59 +0100

    fixed bridge

--- Bridge/src/main/java/de/teamholy/bridge/listener/PlayerJoinListener.java
+++ Bridge/src/main/java/de/teamholy/bridge/listener/PlayerJoinListener.java
@@ -4,11 +4,13 @@ import de.teamholy.bridge.Bridge;
 import de.teamholy.bridge.map.BridgeMapType;
 import de.teamholy.bridge.map.managment.BridgeMapManagment;
 import de.teamholy.bridge.player.management.PlayerManagement;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -19,6 +21,11 @@ import java.util.concurrent.ThreadLocalRandom;
  * Written by charon
  **/
 public class PlayerJoinListener implements Listener {
+
+    @EventHandler
+    public void onSpawn(PlayerSpawnLocationEvent event) {
+        event.setSpawnLocation(new Location(event.getPlayer().getWorld(), 0,0,0));
+    }
 
     @EventHandler
     public void onLogin(PlayerLoginEvent event) {
