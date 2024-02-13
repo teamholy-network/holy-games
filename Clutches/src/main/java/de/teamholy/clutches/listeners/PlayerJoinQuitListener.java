e184b0f3defe61693374755583cafd39dce7b092
charonxyz <68405438+charonxyz@users.noreply.github.com>
2024-02-13 04:16:33 +0100

    Clutches Join: wait 10 Seconds before placing npc's

--- Clutches/src/main/java/de/teamholy/clutches/listeners/PlayerJoinQuitListener.java
+++ Clutches/src/main/java/de/teamholy/clutches/listeners/PlayerJoinQuitListener.java
@@ -37,6 +37,10 @@ public class PlayerJoinQuitListener implements Listener {
             player.teleport(BukkitHolyAPI.getInstance().getLocationManager().getLocation("lobby"));
 
             //04042384-cf5e-4f58-a128-6a87ede461b4
+
+        }, 1);
+
+        Bukkit.getScheduler().runTaskLater(Clutches.getInstance(), () -> {
             BukkitHolyAPI.getInstance().getBukkitCacheHandler().getNpcPlayerHashMap().get(player.getUniqueId()).getNpcs().put("playground", new NPCEntry("§a§lPLAYGROUND", UUID.fromString("04042384-cf5e-4f58-a128-6a87ede461b4"), BukkitHolyAPI.getInstance().getLocationManager().getLocation("playground"), 100, 20, true, true).setPlayer(player));
             if (BukkitHolyAPI.getInstance().getLocationManager().getLocation("reduce") != null)
                 BukkitHolyAPI.getInstance().getBukkitCacheHandler().getNpcPlayerHashMap().get(player.getUniqueId()).getNpcs().put("reduce", new NPCEntry("§f§lREDUCE", UUID.fromString("aa89e99d-843a-4ab2-8221-4fe9bcafd8a8"), BukkitHolyAPI.getInstance().getLocationManager().getLocation("reduce"), 100, 20, true, true).setPlayer(player));
@@ -46,8 +50,7 @@ public class PlayerJoinQuitListener implements Listener {
                 BukkitHolyAPI.getInstance().getBukkitCacheHandler().getNpcPlayerHashMap().get(player.getUniqueId()).getNpcs().put("multireduce", new NPCEntry("§f§lMULTIREDUCE", UUID.fromString("6d40f495-d796-4244-9f45-964cdd7e685a"), BukkitHolyAPI.getInstance().getLocationManager().getLocation("multireduce"), 100, 20, true,true).setPlayer(player));
             if (BukkitHolyAPI.getInstance().getLocationManager().getLocation("diagonalclutch") != null) //"§d" + Bridge.getInstance().getDiagonal().size() + playerEntry.getLanguage().getTranslationByKey("player")
                 BukkitHolyAPI.getInstance().getBukkitCacheHandler().getNpcPlayerHashMap().get(player.getUniqueId()).getNpcs().put("diagonalclutch", new NPCEntry("§f§lDIAGONAL", UUID.fromString("03c55754-08fc-4a12-a451-e517c89a3f91"), BukkitHolyAPI.getInstance().getLocationManager().getLocation("diagonalclutch"), 100, 20, true,true).setPlayer(player));
-
-        }, 1);
+        }, 10);
     }
 
     @EventHandler
