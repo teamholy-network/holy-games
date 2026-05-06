140661bf4a6c26dd8b57fe360110ce1477b0e44e
httpmarco <httpmarco@gmx.de>
2026-05-06 22:07:55 +0200

    chore: Remove Lobby module from project settings

--- Lobby/src/main/java/de/teamholy/lobby/lobbyplayer/LobbyPlayerHandler.java
+++
@@ -1,8 +0,0 @@
-package de.teamholy.lobby.lobbyplayer;
-
-import java.util.HashMap;
-import java.util.UUID;
-
-/* copyright by Yassino */
-public class LobbyPlayerHandler extends HashMap<UUID, LobbyPlayer> {
-}
